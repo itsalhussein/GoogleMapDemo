498c1c8abdca1b918afca8746e13fb40f3fdbcae
Hussein <hussein.elsherief@gmail.com>
2022-10-20 02:55:02 +0200

    - Added google maps to the view controller

--- GoogleMapDemo/ViewController.swift
+++ GoogleMapDemo/ViewController.swift
@@ -6,14 +6,48 @@
 //
 
 import UIKit
+import GoogleMaps
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    let manager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        manager.delegate = self
+        
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
+        GMSServices.provideAPIKey("AIzaSyBNBseZYTXblrQikulrtBX4xld217EkxoI")
+        
+        print("License : \n\n \(GMSServices.openSourceLicenseInfo())")
     }
-
-
+    
+       
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first else { return }
+        
+        let coordinate = location.coordinate
+        
+        // Create a GMSCameraPosition that tells the map to display the
+        // coordinate -33.86,151.20 at zoom level 6.
+        let camera = GMSCameraPosition.camera(withLatitude: coordinate.latitude, longitude: coordinate.longitude, zoom: 10.0)
+        let mapView = GMSMapView.map(withFrame: self.view.frame, camera: camera)
+        self.view.addSubview(mapView)
+        
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: coordinate.latitude, longitude: coordinate.longitude)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
+        
+    }
+    
+    
 }
 
